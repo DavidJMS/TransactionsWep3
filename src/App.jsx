67b33489cd6627da0ade8cf9eb1fd7e2312db38f
Federico Lombardozzi <lombardozzifederico@outlook.com>
2022-06-28 15:04:23 +0200

    styled-components global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { ThemeProvider } from 'styled-components'
 import { AppRouter } from './router/AppRouter'
+import GlobalStyles from './ui/components/styles/Global'
+
+const theme = {
+  colors: {
+    body: '#3E3E3E',
+  }
+}
+
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <>
+      <GlobalStyles />
       <AppRouter />
     </>
+    </ThemeProvider>
   )
 }
 
