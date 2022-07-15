f3c1b637d5243e03f8d270464951b99dc207949f
DavidJMS <david.marabay@gmail.com>
2022-07-14 22:21:03 -0400

    Adding query to create contact

--- src/components/contacts/ContactList.jsx
+++ src/components/contacts/ContactList.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
-import { useQuery } from "@apollo/react-hooks"
+import React, { useEffect } from 'react'
+import { useQuery } from '@apollo/react-hooks'
 
-import { GET_QUERY } from '../../querys/contacts'
+import { GET_CONTACT, CREATE_CONTACT } from '../../querys/contacts'
 
 // Environment Vars
 const VITE_DATA_API_KEY = import.meta.env.VITE_DATA_API_KEY
@@ -10,20 +10,34 @@ const VITE_DATABASE = import.meta.env.VITE_DATABASE
 const VITE_DATA_SOURCE = import.meta.env.VITE_DATA_SOURCE
 
 const ContactList = () => {
-  const { error, loading, data } = useQuery(GET_QUERY, {
+  const { error, loading, data } = useQuery(GET_CONTACT, {
     variables: {
       dataApikey: VITE_DATA_API_KEY,
       datasource: VITE_DATA_SOURCE,
       database: VITE_DATABASE,
       collection: VITE_COLLECTION
-    },
+    }
   })
-  console.log(error, data)
-  console.log('Ejecutado')
+  const { loading2, error2, data2 } = useQuery(CREATE_CONTACT, {
+    variables: {
+      dataApikey: VITE_DATA_API_KEY,
+      datasource: VITE_DATA_SOURCE,
+      database: VITE_DATABASE,
+      collection: VITE_COLLECTION,
+      document: {
+        cryptocurrency_account: '8546546578',
+        full_name: 'Desde web'
+      }
+    }
+  })
+  console.log(error, data, loading, 'Ejecutado!')
+  console.log(error2, data2, loading2, 'Creado!')
 
-  return <>
+  return (
+    <>
 
-  </>
+    </>
+  )
 }
 
 export default ContactList
--- src/querys/contacts.js
+++ src/querys/contacts.js
@@ -1,6 +1,6 @@
-import { gql } from '@apollo/client';
+import { gql } from '@apollo/client'
 
-const GET_QUERY = gql`
+const GET_CONTACT = gql`
   query MyQuery ($dataApikey: String!, $datasource: String!, $database: String!, $collection: String! ) {
     getContacts(
       dataApikey: $dataApikey
@@ -9,8 +9,21 @@ const GET_QUERY = gql`
       dataSource: $datasource
     )
   }
-`;
+`
+
+const CREATE_CONTACT = gql`
+  query MyQuery ($dataApikey: String!, $datasource: String!, $database: String!,$collection: String!, $document: JSON!)  {
+    crateContact(
+      collection: $collection
+      dataApikey: $dataApikey
+      database: $database
+      dataSource: $datasource
+      document: $document
+    )
+  }
+`
 
 export {
-  GET_QUERY
+  GET_CONTACT,
+  CREATE_CONTACT
 }
