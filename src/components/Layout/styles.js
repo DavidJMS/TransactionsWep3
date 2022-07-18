a7be3d28f1b9d8f6d860b0fb374bc90593a0784b
Federico Lombardozzi <lombardozzifederico@outlook.com>
2022-07-18 07:25:31 +0200

    fixing scrollbar sidebar content

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -4,7 +4,11 @@ import { v } from "../../ui/components/styles/Variables";
 
 export const SLayout = styled.div`
     display: flex;
+    
 `
 export const SMain = styled.main`
     padding: calc(${v.xxlSpacing} * 2);
+    overflow-y: auto;   
+    flex: 1;
+    height: 100vh;
 `
