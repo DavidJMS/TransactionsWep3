ff7fa92bd655c07dde691b2dc3d7032b34a374d0
Federico Lombardozzi <lombardozzifederico@outlook.com>
2022-07-18 22:22:36 +0200

    add delete function bottom

--- src/components/contacts/ContactCard.jsx
+++ src/components/contacts/ContactCard.jsx
@@ -1,6 +1,13 @@
 /*eslint-disable*/
 import React from 'react'
-import { CCard, CCardHeading, CCardText } from './styles'
+import { 
+    CCard, 
+    CCardHeading, 
+    CCardText 
+} from './styles'
+
+//my imports
+import DeleteContact from './DeleteContact'
 
 const ContactCard = (props) => {
     return (
@@ -12,6 +19,7 @@ const ContactCard = (props) => {
                 <CCardText>
                     {props.account}
                 </CCardText>
+                <DeleteContact key={props.key}  pepe={props.pepe} />
             </CCard>
         </>
     )
---
+++ src/components/contacts/DeleteContact.jsx
@@ -0,0 +1,38 @@
+/*eslint-disable*/
+import React from 'react'
+import { DELETE_CONTACT } from '../../querys/contacts'
+import { useQuery } from '@apollo/client'
+
+
+// Environment Vars
+const VITE_DATA_API_KEY = import.meta.env.VITE_DATA_API_KEY
+const VITE_COLLECTION = import.meta.env.VITE_COLLECTION
+const VITE_DATABASE = import.meta.env.VITE_DATABASE
+const VITE_DATA_SOURCE = import.meta.env.VITE_DATA_SOURCE
+
+function DeleteContact({ key }) {
+
+    const { error, loading, data } = useQuery(DELETE_CONTACT, {
+        variables: {
+            dataApikey: VITE_DATA_API_KEY,
+            datasource: VITE_DATA_SOURCE,
+            database: VITE_DATABASE,
+            collection: VITE_COLLECTION,   
+            filter: { _id: { $oid: key } }  
+        } 
+    })
+
+    const handleDelete = () => {
+        data.deleteContact
+    }
+
+    return (
+        <>
+            <button onClick={handleDelete}>
+                Delete Contact
+            </button>
+        </>
+    )
+}
+
+export default DeleteContact
