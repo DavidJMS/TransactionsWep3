cce6fdd1778b3d99a1d941f35889b44d3ae439e5
DavidJMS <david.marabay@gmail.com>
2022-07-19 15:06:51 -0400

    modified DeleteContact Component

--- src/components/contacts/DeleteContact.jsx
+++ src/components/contacts/DeleteContact.jsx
@@ -1,7 +1,7 @@
 /*eslint-disable*/
 import React from 'react'
-import { DELETE_CONTACT } from '../../querys/contacts'
-import { useQuery } from '@apollo/client'
+import { DELETE_CONTACT_MUTATION } from '../../querys/contacts'
+import { useMutation } from '@apollo/client'
 
 
 // Environment Vars
@@ -12,7 +12,9 @@ const VITE_DATA_SOURCE = import.meta.env.VITE_DATA_SOURCE
 
 function DeleteContact({ uid }) {
 
-    const { error, loading, data } = useQuery(DELETE_CONTACT, {
+    const [deleteContact, { data3, loading3, error3 }] = useMutation(DELETE_CONTACT_MUTATION)
+
+    const { error, loading, data } = useMutation(DELETE_CONTACT, {
         variables: {
             dataApikey: VITE_DATA_API_KEY,
             datasource: VITE_DATA_SOURCE,
@@ -21,9 +23,16 @@ function DeleteContact({ uid }) {
             filter: { _id: { $oid: uid } }  
         } 
     })
-
     const handleDelete = () => {
-        data.deleteContact
+        deleteContact({
+            variables: {
+                dataApikey: VITE_DATA_API_KEY,
+                datasource: VITE_DATA_SOURCE,
+                database: VITE_DATABASE,
+                collection: VITE_COLLECTION,   
+                filter: { _id: { $oid: uid } }  
+            }
+        })
     }
 
     return (
