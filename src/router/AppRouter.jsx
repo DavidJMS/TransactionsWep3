1f04dbdac7a6f1933b1284992dae8be5b63ca3f7
Daniel Sifontes <danielsifontes99@gmail.com>
2022-07-19 10:21:14 -0400

    private routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,21 +2,20 @@ import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { LoginPage } from '../auth/pages/LoginPage'
-import ContactList from '../components/contacts/ContactList'
 import { ContactsPage } from '../pages/ContactsPage'
 import TransactionsPage from '../pages/TransactionsPage'
+import PrivateRoute from './PrivateRoute'
 
 export const AppRouter = () => {
   return (
-    <>
-      <Routes>
-        <Route path='/login' element={<LoginPage />} />
+    <Routes>
+      <Route path='/login' element={<LoginPage />} />
+      <Route element={<PrivateRoute />}>
         <Route path='/dashboard' element={<TransactionsPage />} />
         <Route path='/contacts' element={<ContactsPage />} />
-        <Route path='/contacts1' element={<ContactList />} />
+      </Route>
 
-        <Route path='/' element={<Navigate to='/login' />} />
-      </Routes>
-    </>
+      <Route path='/' element={<Navigate to='/login' />} />
+    </Routes>
   )
 }
